74da90efec88111d6a5c5b8c170e1de7d24c5ef9
PLKuba <major.plays96@gmail.com>
2023-10-26 00:05:13 +0200

    center summary page

--- lib/pages/game_summary_page.dart
+++ lib/pages/game_summary_page.dart
@@ -10,28 +10,30 @@ class GameSummaryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text('Game summary page'),
-        Text('Correct Swipes: ${swipeSummary.correctCount}'),
-        Text('Mid Swipes: ${swipeSummary.midCount}'),
-        Text('Incorrect Swipes: ${swipeSummary.incorrectCount}'),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.of(entryRootContext).pushNamedAndRemoveUntil(
-                  '/entryPage', (Route<dynamic> route) => false); // this will move me back to entry page
-                  // add route to entry page
-            },
-            child: const Text('Go back to main page')),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                  '/', (Route<dynamic> route) => false); // this will move me back to the start of the swiping game
-                  // add route to entry page
-            },
-            child: const Text('Try again'))
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Game summary page'),
+          Text('Correct Swipes: ${swipeSummary.correctCount}'),
+          Text('Mid Swipes: ${swipeSummary.midCount}'),
+          Text('Incorrect Swipes: ${swipeSummary.incorrectCount}'),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(entryRootContext).pushNamedAndRemoveUntil(
+                    '/entryPage', (Route<dynamic> route) => false); // this will move me back to entry page
+                    // add route to entry page
+              },
+              child: const Text('Go back to main page')),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/', (Route<dynamic> route) => false); // this will move me back to the start of the swiping game
+                    // add route to entry page
+              },
+              child: const Text('Try again'))
+        ],
+      ),
     );
   }
 }
